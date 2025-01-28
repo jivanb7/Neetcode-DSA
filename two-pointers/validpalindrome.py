be6a9c41bd70ab4bb126653320cab77b8801aebb
jivanb7 <bal.jivan@yahoo.com>
2025-01-27 23:58:15 -0800

    two pointer dsa problem solved

---
+++ two-pointers/validpalindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered_string = ''
+        
+        for char in s:
+            if char.isalnum():
+                filtered_string += char.lower()
+        return filtered_string == filtered_string[::-1]

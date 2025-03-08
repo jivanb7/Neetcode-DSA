a2a4401276f2024aa2afc853f5402fd075de3d36
jivanb7 <bal.jivan@yahoo.com>
2025-03-07 23:56:27 -0800

    dsa trees - same tree

---
+++ trees/sametree.py
@@ -0,0 +1,37 @@
+'''
+Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [4,7], q = [4,null,7]
+Output: false
+
+Example 3:
+Input: p = [1,2,3], q = [1,3,2]
+Output: false
+
+Constraints:
+0 <= The number of nodes in both trees <= 100.
+-100 <= Node.val <= 100
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
